44481ee4ab23a6e1ec1454aacea3a5b4c4c9d95e
mholzi <mholzi@gmail.com>
2025-06-13 23:50:12 +0200

    Update config_flow.py

--- custom_components/dashstyle/config_flow.py
+++ custom_components/dashstyle/config_flow.py
@@ -3,6 +3,7 @@ import voluptuous as vol
 from homeassistant import config_entries
 from .const import DOMAIN
 
+@config_entries.HANDLERS.register(DOMAIN)
 class DashStyleConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for DashStyle."""
 
@@ -10,9 +11,15 @@ class DashStyleConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
+        # Abort if an instance of DashStyle is already configured
+        if self._async_current_entries():
+            return self.async_abort(reason="single_instance_allowed")
+
         if user_input is not None:
+            # Create the config entry
             return self.async_create_entry(title=user_input["name"], data=user_input)
 
+        # Show the form to the user
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
